f16476730aefd2538dfb8f5145ed3290ae923005
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-05-01 16:30:03 +0200

    add controller restructure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,44 +17,45 @@ let state = {
 // VIEW
 // -----------------------------------------------------------------------------
 class ShowListView {
-    _data; // data from state/model
-    _parentElement = document.querySelector('#movie-list');
-
+    #data; // data from state/model
+    #parentElement = document.querySelector('#movie-list');
+    _;
     render(data) {
+        console.log(data);
         // Take state from controller
-        this._data = data;
+        this.#data = data;
         // Generate markup
         this.markup = this.markupList();
         // Clear parent element
         this._clear();
         // Insert markup to parent element
-        this._parentElement.insertAdjacentHTML('afterbegin', this.markup);
+        this.#parentElement.insertAdjacentHTML('afterbegin', this.markup);
     }
     _clear() {
-        this._parentElement.innerHTML = '';
+        this.#parentElement.innerHTML = '';
     }
 
     markupList() {
-        return this._data.map(movie => this.markupCard(movie)).join('');
+        return this.#data.map(movie => this.markupCard(movie)).join('');
     }
 
     markupCard(movie) {
         return `
-            <li class="movie-card">
-                <span>${movie.title}</span>
-                <span>${movie.rating}</span>
-                <span>${movie.genre}</span>
-            </li>
+        <li class="movie-card">
+            <h4>${movie.title}</h4>
+            <p>Rating: ${movie.rating}</p>
+            <p>Genre: ${movie.genre}</p>
+        </li>
         `;
     }
     renderSpinner() {
-        this._parentElement.innerHTML = '<div>Loading...</div>';
+        this.#parentElement.innerHTML = '<div>Loading...</div>';
     }
     renderError(message = 'An error has occured!') {
-        this._parentElement.innerHTML = `<div>${message}</div>`;
+        this.#parentElement.innerHTML = `<div>${message}</div>`;
     }
     renderMessage(message = 'No movies found. Please add a movie.') {
-        this._parentElement.innerHTML = `<div>${message}</div>`;
+        this.#parentElement.innerHTML = `<div>${message}</div>`;
     }
 }
 
@@ -64,7 +65,7 @@ class AddMovieView {
     _btnAddMovie = document.querySelector('#add-movie');
     _formAddMovie = document.querySelector('#form-add-movie');
 
-    addHandlerAddMovie(handler) {
+    handleAddMovie(handler) {
         this._btnAddMovie.addEventListener('click', e => {
             e.preventDefault();
             // Get data from form
@@ -85,39 +86,51 @@ const addMovieView = new AddMovieView();
 // -----------------------------------------------------------------------------
 class Controller {
     constructor() {
-        this.controlMovieList = this.controlMovieList.bind(this);
-        this.controlAddMovie = this.controlAddMovie.bind(this);
+        this.addMovie = this.addMovie.bind(this);
 
         // Bind
         this.init();
     }
 
-    controlMovieList() {
-        // TODO: renderSpinner
-        // 1. Get data from model
-        const data = state.movieList;
-        // 2. Render data to view
-        showListView.render(data);
-        // resultsView.render(model.getSearchResultsPage());
+    async loadMovies() {
+        // Get data from localStorage
+        const savedMovies = localStorage.getItem('movies');
+        // Set state to savedMovies or empty array
+        state.movieList = savedMovies ? JSON.parse(savedMovies) : [];
+        // return state
+        return state.movieList;
     }
 
-    controlAddMovie(newMovie) {
-        // TODO: AddMovieView.renderSpinner();
-        // 1. Add data to model
-        state.movieList.push(newMovie);
-        // 2. Render data to view
+    saveMovies() {
+        localStorage.setItem('movies', JSON.stringify(state.movieList));
+    }
+
+    displayMovieList() {
+        // TODO: renderSpinner
         showListView.render(state.movieList);
     }
-    init() {
-        // Event/Bind handlers
-        // 1. Load Movie List
-        // showListView.addHandlerMovieList(this.controlMovieList);
-        this.controlMovieList();
-        // 2. Add Movie
-        addMovieView.addHandlerAddMovie(this.controlAddMovie);
-        // 3. Delete Movie
-        // 4. Edit Movie
-        // 5. Save Movie
+
+    addMovie(newMovie) {
+        // TODO: renderSpinner();
+        state.movieList.push(newMovie);
+        this.saveMovies();
+        showListView.render(state.movieList);
+    }
+
+    handleError(error) {
+        console.error('💥 An Error occured:', error);
+        showListView.renderError(`Error: ${error.message}`);
+    }
+
+    async init() {
+        try {
+            await this.loadMovies();
+            this.displayMovieList();
+            addMovieView.handleAddMovie(this.addMovie);
+        } catch (error) {
+            console.error('Initialization failed:', error);
+            showListView.renderError(`Failed to initialize: ${error.message}`);
+        }
     }
 }
 
